7140123e6e92f5ee570ead8b99503b77e32e35f1
Aditya <144545294+Aditya-0510@users.noreply.github.com>
2026-03-31 17:03:28 +0530

    modified schema and some routes

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { login, register, googleLogin } from "../controller/userController.js";
+import { login, register, googleLogin, setRole, getMe  } from "../controller/userController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 export const userRouter = Router();
 
 userRouter.post("/login", login);
 userRouter.post("/signup", register);
 userRouter.post("/google-login", googleLogin);
+
+userRouter.post("/set-role", authMiddleware, setRole);
+userRouter.get("/me", authMiddleware, getMe);
