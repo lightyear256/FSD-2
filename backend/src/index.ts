ef25ca824552c43cf3bd8f97fd6f971bbc6d9642
Bhagya-vishwakarma-20 <Bhagyavishwakarma88@gmail.com>
2026-04-03 17:18:36 +0530

    feat: cd pipeline

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,10 +7,7 @@ import cors from "cors"
 export const app=express();
 app.use(express.json());
 
-app.use(cors({
-  origin: "*", 
-  credentials: true
-}));
+app.use(cors());
 
 app.get("/",(req:Request,res:Response)=>{
     res.json("server running")
